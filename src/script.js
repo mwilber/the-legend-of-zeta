516abb88b2cfb1c5faaaac50c3279b72126f10f2
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2020-04-24 19:52:43 -0400

    Copy edit

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ export const Script = {
         rock: "Ow!",
         labsign: "Hmm. This may be the entrance to the secret lab.",
         sportmodel: "It's just a weather balloon. Nothing to see here.",
-        jello: "This old jalopy wouldn't get me passed Alpha Centauri.",
+        jello: "This old jalopy wouldn't get me to Alpha Centauri.",
         tr3b: "Never seen this one before. Hmm, it says TR-3B.",
         strawhat: "This one's a real classic. Aww. There's a big hole in the side. That's really going to wreck the resale value."
     }
