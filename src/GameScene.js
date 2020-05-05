6ff9be04d19c1af5bdb69b2166c9dbfc742365af
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2020-05-04 20:54:55 -0400

    Remove tile layers for step 1

--- src/GameScene.js
+++ src/GameScene.js
@@ -27,8 +27,6 @@ export class GameScene extends Phaser.Scene {
 	init(data){ }
 
 	preload() {
-		this.load.image("tiles", "assets/images/Area-51.png");
-		this.load.tilemapTiledJSON("map", "assets/tilemaps/area-51.json");
 		this.animsManager.preload();
 	}
 
@@ -46,27 +44,10 @@ export class GameScene extends Phaser.Scene {
 			image: 'zeta',
 			speed: 225
 		});
-		
-		// Load map json from Tiled
-		const map = this.make.tilemap({ key: 'map' });
-		// settings.tiledKey is the name of the tileset in Tiled
-		const tileset = map.addTilesetImage('Area-51', 'tiles');
-		// layer key is the layer name set in Tiled
-		const backgroundLayer = map.createStaticLayer('Background', tileset, 0, 0);
-		const interactiveLayer = map.createStaticLayer('Interactive', tileset, 0, 0);
-		let overheadLayer = map.createStaticLayer('Overhead', tileset, 0, 0);
-
-		// Place the player above the tile layers
-		this.player.setDepth(10);
-		// Place the overhead layer above everything else
-		overheadLayer.setDepth(20);
 
 		// Set up the main (only?) camera
 		const camera = this.cameras.main;
 		camera.startFollow(this.player);
-		camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-		// Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
-		camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 		
 		// Use the anims manager to set up local sprite animations
 		this.animsManager.create();
