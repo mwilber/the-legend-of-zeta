f1b65104760103285fc0a1ad28d4dae3effe8b39
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2020-05-08 23:37:30 -0400

    Add tilemap layers

--- src/GameScene.js
+++ src/GameScene.js
@@ -26,7 +26,7 @@ export class GameScene extends Phaser.Scene {
 	init(data){ }
 
 	preload() {
-		this.load.image('tiles', 'assets/images/Area-51.png');
+		this.load.image('tiles', 'assets/tilemaps/Area-51.png');
 		this.load.tilemapTiledJSON('map', 'assets/tilemaps/area-51.json');
 		this.animsManager.preload();
 	}
@@ -44,6 +44,20 @@ export class GameScene extends Phaser.Scene {
 			image: 'zeta',
 			speed: 225
 		});
+
+		// Load map json from Tiled
+		const map = this.make.tilemap({ key: 'map' });
+		// settings.tiledKey is the name of the tileset in Tiled
+		const tileset = map.addTilesetImage('Area-51', 'tiles');
+		// layer key is the layer name set in Tiled
+		const backgroundLayer = map.createStaticLayer('Background', tileset, 0, 0);
+		const interactiveLayer = map.createStaticLayer('Interactive', tileset, 0, 0);
+		const overheadLayer = map.createStaticLayer('Overhead', tileset, 0, 0);
+
+		// Place the player above the tile layers
+		this.player.setDepth(10);
+		// Place the overhead layer above everything else
+		overheadLayer.setDepth(20);
 
 		// Set up the main (only?) camera
 		const camera = this.cameras.main;
